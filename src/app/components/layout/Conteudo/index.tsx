d5ac79b61b1c7b30b174736487405bf10422cf05
Paulo Serpa <pauloserpa.r@gmail.com>
2025-05-13 19:48:30 -0300

    feat(components): Cabeçalho, Rodapé, Conteudo

---
+++ src/app/components/layout/Conteudo/index.tsx
@@ -0,0 +1,3 @@
+export const Conteudo = ({ children }: { children: React.ReactNode }) => {
+  return <main className="min-h-screen flex-1 p-4">{children}</main>;
+};
---
+++ src/app/components/layout/Rodape/index.tsx
@@ -0,0 +1,10 @@
+export const Rodape = () => {
+  return (
+    <footer className="flex justify-center bg-white p-4 shadow-md">
+      <p className="text-sm text-gray-600">
+        {new Date().getFullYear()} Sead Ufes. Todos os direitos reservados.
+        Desenvolvido por LDI.
+      </p>
+    </footer>
+  );
+};
