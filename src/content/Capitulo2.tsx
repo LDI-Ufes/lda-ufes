31f6661dfd2e7d4de038cb2a198cba578316ae76
Paulo Serpa <pauloserpa.r@gmail.com>
2025-05-20 19:12:29 -0300

    feat(content): capítulo 2
    Adição de capítulo 2 para testes de conteúdo

---
+++ src/content/Capitulo2.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { Layout } from "@/components/layout/Layout";
+
+const Capitulo2 = () => {
+  return (
+    <Layout titulo="Capítulo 2 - Exemplo" subtitulo="Exemplo">
+      <h1 className="text-2xl font-bold">
+        Bem-vindo ao modelo de livro acessível digital.
+      </h1>
+      <p className="text-gray-600">
+        Este é um modelo de livro acessível digital que utiliza a tecnologia de
+        leitura acessível para tornar o conteúdo mais acessível para todos os
+        usuários.
+      </p>
+      <Button>Clique aqui</Button>
+    </Layout>
+  );
+};
+
+export default Capitulo2;
