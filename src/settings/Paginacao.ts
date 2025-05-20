2dc97907fa198b20b989a069a51113a42e06c0d6
Paulo Serpa <pauloserpa.r@gmail.com>
2025-05-20 18:53:32 -0300

    refactor(settings): Paginacao
    Componente para determinar sequência de páginas do livro. Similar com o arquivo , no entanto, possui uma sintaxe mais simples

---
+++ src/settings/Paginacao.ts
@@ -0,0 +1,12 @@
+import type { ConfigPaginacao } from "@/app/router/types";
+
+export const Paginacao: ConfigPaginacao[] = [
+  {
+    title: "Capítulo 1",
+    page: "Capitulo1",
+  },
+  {
+    title: "Capítulo 2",
+    page: "Capitulo2",
+  },
+];
