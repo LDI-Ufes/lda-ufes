a7422300876ddac731b9b77757a6b4a84a11ea3f
Paulo Serpa <pauloserpa.r@gmail.com>
2025-05-24 12:14:39 -0300

    feat(components): configurações de botões e links

--- src/app/components/ui/button.tsx
+++ src/app/components/ui/button.tsx
@@ -5,19 +5,22 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/app/utils/cn";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 cursor-pointer",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 cursor-pointer border-2 border-transparent",
   {
     variants: {
       variant: {
-        default: "bg-blue-500 text-white hover:bg-blue-600",
-        destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        default: "bg-background text-white hover:bg-blue-600",
+        outline: "bg-background hover:bg-accent hover:text-accent-foreground",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+      },
+      variantColor: {
+        primary:
+          "text-white bg-blue-600 hover:bg-blue-700 hover:border-blue-700 [&>svg]:text-white",
+        secondary:
+          "text-red-700 hover:bg-red-900 border-red-700 hover:text-white [&>svg]:text-red-700",
+        destructive:
+          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
       },
       size: {
         default: "h-10 px-4 py-2",
@@ -25,10 +28,16 @@ const buttonVariants = cva(
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
       },
+      textStyle: {
+        default: "text-normal",
+        uppercase: "uppercase",
+      },
     },
     defaultVariants: {
       variant: "default",
+      variantColor: "primary",
       size: "default",
+      textStyle: "default",
     },
   },
 );
@@ -40,11 +49,24 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  (
+    {
+      className,
+      variant,
+      size,
+      asChild = false,
+      textStyle,
+      variantColor,
+      ...props
+    },
+    ref,
+  ) => {
     const Comp = asChild ? Slot : "button";
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(
+          buttonVariants({ variant, size, className, textStyle, variantColor }),
+        )}
         ref={ref}
         {...props}
       />
