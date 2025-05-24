098e5ae114e26a85f8c91e9defb497c8960988d1
Paulo Serpa <pauloserpa.r@gmail.com>
2025-05-24 12:16:12 -0300

    feat(components): page layout props
    Adição das propriedades de subtitulo e capa

--- src/app/components/layout/Page/index.tsx
+++ src/app/components/layout/Page/index.tsx
@@ -7,21 +7,23 @@ import { Sumario } from "../Summary";
 interface PageProps {
   children: ReactNode;
   title: string;
-  subtitle: string;
+  subtitle?: string;
   hasSummary?: boolean;
+  cover?: string;
 }
 
 export const Page = ({
   children,
   title,
-  subtitle,
+  subtitle = "Livro Digital",
   hasSummary = true,
+  cover,
 }: PageProps) => {
   return (
     <>
       <Header title={title} subtitle={subtitle} />
-      <main className="mx-auto flex max-w-3xl gap-8">
-        <Content>{children}</Content>
+      <main className="mx-auto flex max-w-7xl justify-between gap-8">
+        <Content cover={cover}>{children}</Content>
         {hasSummary && <Sumario />}
       </main>
       <Footer />
