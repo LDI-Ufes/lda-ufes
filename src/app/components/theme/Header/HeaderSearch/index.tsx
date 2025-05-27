4056c3c8f7faf2e078bf6d0db08ecdd05c2f8ab1
Paulo Serpa <pauloserpa.r@gmail.com>
2025-05-27 19:40:57 -0300

    fix(components): botão de pesquisa do header
    Melhorias na funcionalidade de visualização da barra de pesquisa

--- src/app/components/theme/Header/HeaderSearch/index.tsx
+++ src/app/components/theme/Header/HeaderSearch/index.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/app/components/ui/Button";
 import { cn } from "@/app/utils";
 import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
+import { MdClose } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 
 const HeaderSearch = ({ className }: { className?: string }) => {
@@ -13,6 +14,24 @@ const HeaderSearch = ({ className }: { className?: string }) => {
     if (value.trim()) {
       navigate(`/pesquisa?q=${value}`);
       setIsMobileSearchVisible(false);
+    }
+  };
+
+  const handleIconClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const isLargeScreen = window.innerWidth >= 1024;
+    if (isLargeScreen) {
+      if (value.trim()) {
+        handleSearch();
+      }
+    } else {
+      if (!isMobileSearchVisible) {
+        setIsMobileSearchVisible(true);
+      } else {
+        if (value.trim()) {
+          handleSearch();
+        }
+      }
     }
   };
 
@@ -34,14 +53,7 @@ const HeaderSearch = ({ className }: { className?: string }) => {
         <Button
           variant="ghost"
           className="max-lg:border-transparent lg:absolute lg:top-0 lg:right-0 lg:border-none"
-          onClick={
-            value
-              ? (e) => {
-                  e.preventDefault();
-                  handleSearch();
-                }
-              : undefined
-          }
+          onClick={handleIconClick}
         >
           <FiSearch
             className={cn(
@@ -54,12 +66,12 @@ const HeaderSearch = ({ className }: { className?: string }) => {
       </div>
 
       {isMobileSearchVisible && (
-        <div className="bg-theme-background absolute top-full left-0 z-20 w-full p-4 shadow-md lg:hidden">
+        <div className="bg-theme-background absolute top-3 left-0 z-20 w-full p-2 px-4 lg:hidden">
           <div className="relative">
             <input
               type="text"
               placeholder="Buscar"
-              className="h-10 w-full flex-1 rounded-lg border-2 border-slate-400 p-4 pr-12 focus:border-transparent focus:ring-2 focus:ring-blue-500"
+              className="h-10 w-full flex-1 rounded-lg border-2 border-slate-400 p-4 ps-10 pr-24 focus:border-transparent focus:ring-2 focus:ring-blue-500"
               value={value}
               onChange={(e) => setValue(e.target.value)}
               onKeyDown={(e) => {
@@ -71,7 +83,7 @@ const HeaderSearch = ({ className }: { className?: string }) => {
             />
             <Button
               variant="ghost"
-              className="absolute top-0 right-0 h-full border-none px-3"
+              className="absolute top-1/2 left-0 h-10 -translate-y-1/2 border-none px-3"
               onClick={
                 value
                   ? (e) => {
@@ -84,6 +96,21 @@ const HeaderSearch = ({ className }: { className?: string }) => {
               <FiSearch
                 className={cn(
                   "size-5 text-slate-500",
+                  value && "text-secondary",
+                )}
+              />
+            </Button>
+            <Button
+              variant="ghost"
+              className="absolute top-1/2 right-0 h-10 -translate-y-1/2 border-none px-3"
+              onClick={() => {
+                setValue("");
+                setIsMobileSearchVisible(false);
+              }}
+            >
+              <MdClose
+                className={cn(
+                  "text-theme-text size-5",
                   value && "text-secondary",
                 )}
               />
