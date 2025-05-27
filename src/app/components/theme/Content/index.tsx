7bff672dc85afc52462672b8ede8d523e8c73fa4
Paulo Serpa <pauloserpa.r@gmail.com>
2025-05-27 18:46:33 -0300

    refactor(components): conteúdo do tema
    Para evitar que conteúdos excedam o limite da largura do componente, foi adicionado um overflow no eixo lateral

--- src/app/components/theme/Content/index.tsx
+++ src/app/components/theme/Content/index.tsx
@@ -3,7 +3,7 @@ import { Navigation } from "../Navigation";
 export const Content = ({ children }: { children: React.ReactNode }) => {
   return (
     <div
-      className="text-theme-text min-h-[80dvh] max-w-3xl flex-1 px-5 py-5"
+      className="text-theme-text min-h-[80dvh] w-full max-w-3xl flex-1 overflow-x-hidden px-5 py-5"
       id="content"
     >
       {children}
