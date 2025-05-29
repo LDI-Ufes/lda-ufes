edb9e60f7b7f341af64bcedbe83c0f6fdc77e8ca
Paulo Serpa <pauloserpa.r@gmail.com>
2025-05-29 12:08:43 -0300

    refactor(styles): desacoplando variáveis CSS
    Reorganizando acessos e configurações necessárias para a funcionalidade básica do livro. Agora o usuário poderá altera as variáveis que ele deseja com base na pasta interna do projeto

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "../styles/index.css";
+import "./styles/app.css";
 import { OnboardingProvider } from "@/app/contexts";
 import { Onboarding } from "@/app/components/onboarding";
 import Router from "@/app/router";
