05c133998aba220ba16b059f46953bda46ee05b9
liviascopel <livia.scopel@edu.ufes.br>
2025-05-30 09:34:47 -0300

    fix(equation): correção de estilo e layout do componente equation

--- src/app/components/ui/Equation/index.tsx
+++ src/app/components/ui/Equation/index.tsx
@@ -8,10 +8,12 @@ interface EquationProps {
 
 const Equation: React.FC<EquationProps> = ({ id, imgSrc, children }) => {
   return (
-    <div className="formula">
-      <div className="line">
-        {imgSrc ? <img className="eq" src={imgSrc} /> : <p>{children}</p>}
-        <span className="box-eq">equação {id}</span>
+    <div className="formula mt-4">
+      <div className="line flex flex-col items-start">
+        <div className="border-2 border-secondary rounded-lg p-2">
+          {imgSrc ? <img className="eq" src={imgSrc} /> : children}
+        </div>
+        <span className="box-eq mt-1 text-sm mb-4">equação {id}</span>
       </div>
     </div>
   );
