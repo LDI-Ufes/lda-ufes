caefd2d3a226f99bc92c494ab81b14926c296053
liviascopel <livia.scopel@edu.ufes.br>
2025-05-29 22:52:32 -0300

    feat(ui): responsividade no componente figure

--- src/app/components/ui/Figure/index.tsx
+++ src/app/components/ui/Figure/index.tsx
@@ -9,9 +9,9 @@ interface FigureProps {
 
 const Figure: React.FC<FigureProps> = ({ imgSrc, alt, children, figureNumber }) => {
   return (
-    <figure className="my-8">
-      <img src={imgSrc} alt={alt} />
-      <figcaption><span className="text-primary font-semibold">Figura {figureNumber} </span>{children}</figcaption>
+    <figure className="my-8 mx-auto max-w-3xl">
+      <img src={imgSrc} alt={alt} className="w-full h-auto rounded-md" />
+      <figcaption className="text-sm md:text-base mt-2"><span className="text-primary font-semibold">Figura {figureNumber} </span>{children}</figcaption>
     </figure>
   );
 };
