429a816741ba36870c9360c680776db89213996b
Paulo Serpa <pauloserpa.r@gmail.com>
2025-05-31 12:59:17 -0300

    feat: types e constants

--- src/app/hooks/useTheme/ThemeColor/constants.d.ts
+++ src/app/hooks/useTheme/ThemeColor/constants.d.ts
@@ -1,1 +1,12 @@
 export const THEME_COLORS = ["light", "dark", "cream"] as const;
+
+export const THEME_TARGETS = {
+  dark: {
+    background: "#161616",
+    text: "#dbdbdb",
+  },
+  cream: {
+    background: "#f0e6d2",
+    text: "#2c2c2c",
+  },
+} as const;
---
+++ src/app/hooks/useTheme/ThemeColor/types.d.ts
@@ -0,0 +1,13 @@
+export interface BaseColors {
+  primary: string;
+  secondary: string;
+  text: string;
+  background: string;
+}
+
+export interface CalculatedThemeColors {
+  primary: string;
+  secondary: string;
+  theme: string;
+  themeBackground: string;
+}
