53e7fb2f29f3013c431093920e0ef23e4efd8e8f
Paulo Serpa <pauloserpa.r@gmail.com>
2025-06-02 11:28:02 -0300

    refactor(theme): botão de sumário mobile
    Retirar efeito de aparição condicional com base na altura da tela. Se o componente só é utilziado  no Mobile, não há necessidade

--- src/app/components/theme/ButtonActionSummary/index.tsx
+++ src/app/components/theme/ButtonActionSummary/index.tsx
@@ -1,20 +1,11 @@
 import { Button } from "@/app/components/ui/Button";
 import { MdClose, MdManageSearch } from "react-icons/md";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { cn } from "@/app/utils/cn";
 import { Summary } from "@/app/components/theme/Summary";
 
 const ButtonActionSummary = () => {
-  const [isVisible, setIsVisible] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setIsVisible(window.scrollY > window.innerHeight / 2);
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
 
   const handleOpenModal = () => {
     setIsModalOpen((prev) => !prev);
@@ -26,11 +17,9 @@ const ButtonActionSummary = () => {
         variant="secondary"
         size="icon"
         className={cn(
-          "[disabled]:pointer-events fixed right-4 bottom-8 z-[51] translate-y-4 transition-all duration-150 ease-in-out lg:hidden",
-          isVisible ? "visible opacity-100" : "invisible opacity-0",
+          "[disabled]:pointer-events fixed right-4 bottom-8 z-[51] lg:hidden",
         )}
         onClick={handleOpenModal}
-        disabled={!isVisible}
       >
         {isModalOpen ? (
           <MdClose className="size-5" />
