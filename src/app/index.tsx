211c01e6aed12abcf917a27bfe88cf19c07c0d8e
Paulo Serpa <pauloserpa.r@gmail.com>
2025-06-02 11:14:57 -0300

    feat: clarity plugin

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -7,10 +7,16 @@ import { OnboardingProvider, ThemeProvider } from "@/app/contexts";
 import { Onboarding } from "@/app/components/onboarding";
 import Router from "@/app/router";
 import { Loader } from "@/app/components/layout/Loader";
+import Clarity from "@microsoft/clarity";
+
 const root = document.getElementById("root");
 
 if (!root) {
   throw new Error("Elemento root não encontrado");
+}
+
+if (import.meta.env.VITE_APP_CLARITY_PROJECT_ID) {
+  Clarity.init(import.meta.env.VITE_APP_CLARITY_PROJECT_ID);
 }
 
 createRoot(root).render(
